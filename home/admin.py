c416ad188e4442c570877d3b2e2cb161a5b82bc7
Amirjfn <amir666640@gmail.com>
2024-05-03 23:23:24 +0430

    commit all

---
+++ home/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Category, Product
+
+
+admin.site.register(Category)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	raw_id_fields = ('category',)
+
+
